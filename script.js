1c3ace7132bd2f2f1e6eb5aef33d215dac80ec4e
Kozim <kozim_7484443@mail.ru>
2022-06-22 15:57:40 +0500

    первая запись

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const numberOfFilms = "Сколько фильмов вы уже посмотрели";
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+alert("Один из последних просмотренных фильмов?");
+alert("На сколько оцените его?");
